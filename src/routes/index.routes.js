5e4c3c76e7acdd81a80425f51c7eb7e1735acefa
Sugam Neupane <neupane.sugam.19.sn@gmail.com>
2026-06-13 11:11:44 +0545

    fix: analytics route path

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes.js";
 import taskRoutes from "./task.routes.js";
-
+import analyticsRoutes from "./analytics.routes.js";
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/tasks", taskRoutes);
+router.use("/analytics", analyticsRoutes);
 
 export default router;
