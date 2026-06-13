ae1301daa0b6647becaa066883369f2ab2d235b4
Sugam Neupane <neupane.sugam.19.sn@gmail.com>
2026-06-13 11:43:15 +0545

    fix: cookies settings for prod

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve("src/public")));
-
+app.set("trust proxy", 1);
 app.use(sessionMiddleware);
 app.use(rateLimiter);
 
